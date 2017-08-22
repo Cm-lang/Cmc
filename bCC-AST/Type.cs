4295667765c4cc4a99d9615c4bb006497edc2660
ice1000 <ice1000kotlin@foxmail.com>
2017-08-22 20:37:12 +0800

    add primary type

--- bCC-AST/Type.cs
+++ bCC-AST/Type.cs
@@ -18,6 +18,22 @@ namespace bCC_AST
 		}
 	}
 
+	public class PrimaryType : Type
+	{
+		public readonly Type Container;
+		public readonly Type Parameter;
+
+		public PrimaryType(Type container, Type parameter) : base(PrimaryTypeToString(container, parameter))
+		{
+			Container = container;
+			Parameter = parameter;
+		}
+
+		public static string PrimaryTypeToString(Type args, Type ret) => args + "<" + ret + ">";
+
+		public override string ToString() => PrimaryTypeToString(Container, Parameter);
+	}
+
 	public class LambdaType : Type
 	{
 		public readonly IList<Type> ArgsList;
