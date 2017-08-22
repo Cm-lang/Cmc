89d4ecd596a4aafb3dd29c58b1a4b40a5941ef68
ice1000 <ice1000kotlin@foxmail.com>
2017-08-22 16:00:10 +0800

    add ignore, add basic codes

---
+++ bCC-AST/Ast.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace bCC_AST
+{
+	public interface IAst
+	{
+	}
+
+	public abstract class Statement : IAst
+	{
+	}
+
+	public class StatementList : Statement
+	{
+		public readonly IList<Statement> Statements = new List<Statement>();
+	}
+
+	public class FunctionDeclaration : Statement
+	{
+		
+	}
+}
