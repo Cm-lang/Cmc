d7b1eb51b43f1aa87a215e9a15c277aaa8813c7b
ice1000 <ice1000kotlin@foxmail.com>
2017-08-22 16:31:37 +0800

    update

--- bCC-AST/Ast.cs
+++ bCC-AST/Ast.cs
@@ -1,20 +1,30 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace bCC_AST
 {
-	public interface IAst
+	public abstract class IAst
 	{
+		public MetaData MetaData;
+
+		protected IAst(MetaData metaData)
+		{
+			MetaData = metaData;
+		}
 	}
 
 	public class Statement : IAst
 	{
+		public Statement(MetaData metaData) : base(metaData)
+		{
+		}
 	}
 
 	public class StatementList : Statement
 	{
 		public readonly IList<Statement> Statements;
 
-		public StatementList(params Statement[] statements)
+		public StatementList(MetaData metaData, params Statement[] statements) : base(metaData)
 		{
 			Statements = statements;
 		}
@@ -22,13 +32,63 @@ namespace bCC_AST
 
 	public abstract class Declaration : IAst
 	{
+		public abstract IList<Declaration> FindDependencies();
+
+		protected Declaration(MetaData metaData) : base(metaData)
+		{
+		}
 	}
 
 	public class FunctionDeclaration : Declaration
 	{
+		public override IList<Declaration> FindDependencies()
+		{
+			throw new NotImplementedException();
+		}
+
+		public FunctionDeclaration(MetaData metaData) : base(metaData)
+		{
+		}
 	}
 
 	public class VariableDeclaration : Declaration
 	{
+		public override IList<Declaration> FindDependencies()
+		{
+			throw new NotImplementedException();
+		}
+
+		public VariableDeclaration(MetaData metaData) : base(metaData)
+		{
+		}
+	}
+
+	public class Macro : Declaration
+	{
+		public override IList<Declaration> FindDependencies() => new List<Declaration>();
+
+		public Macro(MetaData metaData) : base(metaData)
+		{
+		}
+	}
+
+	public class BcFile
+	{
+		public readonly IList<Declaration> Declarations;
+
+		public BcFile(params Declaration[] declarations)
+		{
+			Declarations = declarations;
+			foreach (var declaration in Declarations)
+			{
+				foreach (var dependency in declaration.FindDependencies())
+				{
+					if (!Declarations.Contains(dependency))
+					{
+						Console.WriteLine("Error: ");
+					}
+				}
+			}
+		}
 	}
 }
---
+++ bCC-AST/MetaData.cs
@@ -0,0 +1,8 @@
+﻿namespace bCC_AST
+{
+	public struct MetaData
+	{
+		public int LineNumber;
+		public string FileName;
+	}
+}
