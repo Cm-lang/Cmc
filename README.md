1bc569c5531b456dd5b8a9d49c59b12ceadfb6a0
ice1000 <ice1000kotlin@foxmail.com>
2017-08-23 12:51:20 +0800

    why!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- README.md
+++ README.md
@@ -6,5 +6,6 @@ Travis CI|[![Build Status](https://travis-ci.org/bC-Programming-Language-Standar
 
 [![](https://img.shields.io/badge/bC--lang-Compiler-blue.svg)](https://github.com/bC-Programming-Language-StandardCouncil/bC-Compiler)
 [![license](https://img.shields.io/github/license/bC-Programming-Language-StandardCouncil/bC-Compiler.svg)](https://github.com/bC-Programming-Language-StandardCouncil/bC-Compiler)
+[![Gitter](https://badges.gitter.im/bC-Programming-Language-StandardCouncil/bC-Compiler.svg)](https://gitter.im/bC-Programming-Language-StandardCouncil/bC-Compiler?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge)
 
 The compiler for bC-language.
--- bCC/Environment.cs
+++ bCC/Environment.cs
@@ -30,21 +30,11 @@ namespace bCC
 		public Declaration FindDeclarationByName(string name)
 		{
 			var env = this;
-			// I don't know why I must declare a variable here
-			// I think I can return the local object directly but it fails
-			Declaration ret = null;
 			do
-			{
 				foreach (var declaration in env.Declarations)
 					if (string.Equals(declaration.Name, name, StringComparison.Ordinal))
-					{
-						Console.WriteLine(declaration.Name + ", " + name);
-						ret = declaration;
-						goto end;
-					}
-			} while ((env = env.Outer) != null);
-			end:
-			return ret;
+						return declaration; while ((env = env.Outer) != null);
+			return null;
 		}
 	}
 }
