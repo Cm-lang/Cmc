c023d9fce2878e9bfc9bc4e9f826d2746e35f73f
ice1000 <ice1000kotlin@foxmail.com>
2017-08-28 22:56:49 +0800

    add tools

---
+++ Tools/CommandLine.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+
+namespace Tools
+{
+	public static class CommandLine
+	{
+		public static void RunCommand(string command)
+		{
+			var cmd = new Process
+			{
+				StartInfo =
+				{
+					FileName = "cmd.exe",
+					RedirectStandardInput = true,
+					RedirectStandardOutput = true,
+					CreateNoWindow = true,
+					UseShellExecute = false
+				}
+			};
+			cmd.Start();
+
+			cmd.StandardInput.WriteLine(command);
+			cmd.StandardInput.Flush();
+			cmd.StandardInput.Close();
+			cmd.WaitForExit();
+//			Console.WriteLine(cmd.StandardOutput.ReadToEnd());
+		}
+	}
+}
