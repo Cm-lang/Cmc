a5b7fc24a1a94c891d6c2442b245433e3ba85b3b
ice1000 <ice1000kotlin@foxmail.com>
2017-08-29 16:30:28 +0800

    test pass

--- bCC/Declaration.cs
+++ bCC/Declaration.cs
@@ -57,7 +57,7 @@ namespace bCC
 			// FEATURE #30
 			Type.SurroundWith(Env);
 			if (Type is UnknownType unknownType) Type = unknownType.Resolve();
-			if (Type is PrimaryType primaryType) Align = primaryType.
+			if (Type is PrimaryType primaryType) Align = primaryType.Align;
 			// FEATURE #11
 			if (!string.Equals(exprType.ToString(), NullType, Ordinal) &&
 			    !Equals(Type, exprType))
