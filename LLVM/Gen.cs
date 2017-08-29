83d5fcd3e548dea984870d1afa14f7195bf0b1ad
ice1000 <ice1000kotlin@foxmail.com>
2017-08-30 01:11:15 +0800

    use gen ast holder

--- LLVM/Gen.cs
+++ LLVM/Gen.cs
@@ -3,10 +3,9 @@ using System.Text;
 using bCC;
 using bCC.Core;
 using bCC.Expression;
-using bCC.Statement;
 using JetBrains.Annotations;
 using Tools;
-using static LLVM.TypeConverter;
+using static LLVM.GenAstHolder;
 
 namespace LLVM
 {
@@ -43,79 +42,6 @@ namespace LLVM
 			File.WriteAllText(outputFile, Generate(declarations));
 			CommandLine.RunCommand($"llc-4.0 {outputFile}.ll -filetype=obj");
 			CommandLine.RunCommand($"gcc {outputFile}.ll -o {outputFile}");
-		}
-
-		/// <summary>
-		///  generate llvm ir by the given ast
-		/// </summary>
-		/// <param name="builder">the string builder used to append ir</param>
-		/// <param name="element">the ast element waiting to be generated</param>
-		/// <param name="varName"></param>
-		public static void GenAst(
-			[NotNull] StringBuilder builder,
-			[NotNull] Ast element,
-			ref ulong varName)
-		{
-			if (element is LiteralExpression expression)
-			{
-				if (expression is StringLiteralExpression str)
-					builder.AppendLine(
-						$"store i8* getelementptr inbounds ([{str.Length} x i8]," +
-						$"[{str.Length} x i8]* @.str{str.ConstantPoolIndex}, i32 0, i32 0)," +
-						$"i8** %{varName}, align 8");
-				else if (expression is IntLiteralExpression integer)
-					builder.AppendLine(
-						$"store {integer.Type} {integer.Value}, {integer.Type}* %{varName}, align {integer.Type.Align}");
-			}
-			if (element is TypeDeclaration typeDeclaration)
-				builder.AppendLine($"; type alias: <{typeDeclaration.Name}> -> <{typeDeclaration.Type}>");
-			else if (element is VariableDeclaration variable)
-			{
-				if (variable.IsGlobal)
-				{
-					builder.Append($"@{varName}=global {ConvertType(variable.Type)} ");
-					if (variable.Expression is IntLiteralExpression integer)
-						builder.Append(
-							$"{integer.Value}");
-					else if (variable.Expression is BoolLiteralExpression boolean)
-						builder.Append(
-							$"{boolean.ValueToInt()}");
-					else if (variable.Expression is StringLiteralExpression str)
-						builder.Append(
-							$"i8* getelementptr inbounds ([{str.Length} x i8], [{str.Length} x i8]* " +
-							$"@.str{str.ConstantPoolIndex}, i32 0, i32 0),");
-					builder.AppendLine($", align {variable.Align}");
-					// TODO deal with other types
-					variable.Address = varName;
-					// TODO for complex initialization, generate a function to do this job
-					varName++;
-				}
-				else
-				{
-					builder.AppendLine(
-						$"%{varName} = alloca {ConvertType(variable.Type)}, align {variable.Align}");
-					variable.Address = varName;
-					GenAst(builder, variable.Expression, ref varName);
-					varName++;
-				}
-			}
-			else if (element is ReturnStatement returnStatement)
-			{
-				var expr = returnStatement.Expression;
-				GenAst(builder, expr, ref varName);
-				builder.AppendLine(
-					$"ret {ConvertType(expr.GetExpressionType())} %{varName}");
-				varName++;
-			}
-			else if (element is StatementList statements)
-			{
-				ulong localVarCount = 0;
-				foreach (var statement in statements.Statements)
-				{
-					GenAst(builder, statement, ref localVarCount);
-					localVarCount++;
-				}
-			}
 		}
 	}
 }
