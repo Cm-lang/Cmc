baa52659a11963af6d0e3f2dc20b88e0505c1039
ice1000 <ice1000kotlin@foxmail.com>
2017-09-01 18:26:36 +0800

    finished renaming

--- Cmc/Properties/AssemblyInfo.cs
+++ Cmc/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("bCC")]
+[assembly: AssemblyTitle("Cmc")]
 [assembly: AssemblyDescription("The analyzer of bC language")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("bCC")]
+[assembly: AssemblyProduct("Cmc")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
