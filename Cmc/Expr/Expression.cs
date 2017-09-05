fd1f1cd0a270c2c1cd8fb686e2d122cac20f8c91
ice1000 <ice1000kotlin@foxmail.com>
2017-09-05 21:53:48 +0800

    move files, update llvm codegen

--- Cmc/Expr/Expression.cs
+++ Cmc/Expr/Expression.cs
@@ -44,64 +44,6 @@ namespace Cmc.Expr
 		}
 	}
 
-	public class LiteralExpression : AtomicExpression
-	{
-		[NotNull] public readonly Type Type;
-
-		protected LiteralExpression(
-			MetaData metaData,
-			[NotNull] Type type) :
-			base(metaData) => Type = type;
-
-		public override Type GetExpressionType() => Type;
-	}
-
-	public class IntLiteralExpression : LiteralExpression
-	{
-		public static readonly int[] AcceptableLength = {8, 16, 32, 64};
-		public readonly int Length;
-
-		[NotNull] public readonly string Value;
-
-		public IntLiteralExpression(
-			MetaData metaData,
-			[NotNull] string value,
-			// FEATURE #27
-			bool isSigned,
-			int length = 32)
-			: base(metaData, new PrimaryType(metaData, $"{(isSigned ? "i" : "u")}{length}", length / 8))
-		{
-			Value = value;
-			Length = length;
-			// FEATURE #26
-			if (!AcceptableLength.Contains(length))
-				Errors.Add($"{MetaData.GetErrorHeader()}integer length of {length} is not supported");
-		}
-
-		public override IEnumerable<string> Dump() =>
-			new[] {$"literal expression [{Value}]:\n"}
-				.Concat(Type.Dump().Select(MapFunc));
-	}
-
-	public class BoolLiteralExpression : LiteralExpression
-	{
-		public readonly bool Value;
-
-		public BoolLiteralExpression(
-			MetaData metaData,
-			bool value) :
-			base(metaData, new PrimaryType(
-				metaData,
-				PrimaryType.BoolType,
-				1))
-			=> Value = value;
-
-		public int ValueToInt() => Value ? 1 : 0;
-
-		public override IEnumerable<string> Dump() =>
-			new[] {$"bool literal expression [{Value}]:\n"};
-	}
-
 	public class StringLiteralExpression : Expression
 	{
 		public readonly int ConstantPoolIndex;
@@ -204,6 +146,9 @@ namespace Cmc.Expr
 		public override VariableExpression GetLhsExpression() => this;
 	}
 
+	/// <summary>
+	///  Idris-style hole-oriented programming
+	/// </summary>
 	public sealed class HoleExpression : Expression
 	{
 		public HoleExpression(MetaData metaData) : base(metaData)
--- LLVM/GenExpression.cs
+++ LLVM/GenExpression.cs
@@ -6,6 +6,22 @@ namespace LLVM
 {
 	public static class GenExpression
 	{
+		private static void StoreAtomicExpression(
+			[NotNull] StringBuilder builder,
+			[NotNull] AtomicExpression expression,
+			ref ulong varName)
+		{
+			if (expression is IntLiteralExpression integer)
+				builder.AppendLine(
+					$"  store {integer.Type} {integer.Value}, {integer.Type}* %{varName}, align {integer.Type.Align}");
+			else if (expression is BoolLiteralExpression boolean)
+				builder.AppendLine(
+					$"  store i8 {boolean.ValueToInt()}, i8* %{varName}, align 1");
+			else if (expression is VariableExpression variable)
+				builder.AppendLine(
+					$"  store {variable.GetExpressionType()} %{}, {}, align {variable.GetExpressionType().Align}");
+		}
+
 		public static void GenAstExpression(
 			[NotNull] StringBuilder builder,
 			[NotNull] Expression element,
@@ -16,16 +32,8 @@ namespace LLVM
 					$"  store i8* getelementptr inbounds ([{str.Length} x i8]," +
 					$"[{str.Length} x i8]* @.str{str.ConstantPoolIndex}, i32 0, i32 0)," +
 					$"i8** %{varName}, align 8");
-			else // ReSharper disable once InvertIf
-			if (element is LiteralExpression expression)
-			{
-				if (expression is IntLiteralExpression integer)
-					builder.AppendLine(
-						$"  store {integer.Type} {integer.Value}, {integer.Type}* %{varName}, align {integer.Type.Align}");
-				else if (expression is BoolLiteralExpression boolean)
-					builder.AppendLine(
-						$"  store i8 {boolean.ValueToInt()}, i8* %{varName}, align 1");
-			}
+			else if (element is AtomicExpression expression)
+				StoreAtomicExpression(builder, expression, ref varName);
 			else if (element is FunctionCallExpression functionCall)
 			{
 				// TODO localize paramters
