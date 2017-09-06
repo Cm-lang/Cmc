9f3b718c554e104f2d88b911eac144c95e884f50
ice1000 <ice1000kotlin@foxmail.com>
2017-09-07 01:43:51 +0800

    add proj

---
+++ CmcExec/Executable.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using Cmc.Core;
+using Cmc.Decl;
+using Cmc.Expr;
+using Cmc.Stmt;
+using LLVM;
+
+namespace CmcExec
+{
+	internal class Executable
+	{
+		public static void Main(string[] args)
+		{
+			Gen.RunLlvm(
+				"out.exe",
+				new VariableDeclaration(MetaData.Empty,
+					"i", new IntLiteralExpression(MetaData.Empty, "1", true)),
+				new VariableDeclaration(MetaData.Empty,
+					"j", new StringLiteralExpression(MetaData.Empty, "boy next door")),
+				new VariableDeclaration(MetaData.Empty,
+					"main", new LambdaExpression(MetaData.Empty,
+						new StatementList(MetaData.Empty,
+							new VariableDeclaration(MetaData.Empty,
+								"j", new StringLiteralExpression(MetaData.Empty, "boy next door")),
+							new ExpressionStatement(MetaData.Empty,
+								new FunctionCallExpression(MetaData.Empty,
+									new VariableExpression(MetaData.Empty, "print"),
+									new List<Expression>(new[]
+									{
+										new VariableExpression(MetaData.Empty, "j")
+									}))),
+							new ReturnStatement(MetaData.Empty,
+								new IntLiteralExpression(MetaData.Empty, "0", true)))))
+			);
+		}
+	}
+}
