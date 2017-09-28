6a2a7d467dccf6e7cbec9a73708deab97c873ee1
ice1000 <ice1000kotlin@foxmail.com>
2017-09-28 23:34:17 +0800

    add interpreter

---
+++ Interpreter/CmInterpreter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Cmc;
+using Cmc.Core;
+using Cmc.Decl;
+
+namespace Interpreter
+{
+	public class CmInterpreter
+	{
+		public static Environment InitEnv()
+		{
+			var planet = new Environment(Environment.SolarSystem);
+			planet.Declarations.Add(new VariableDeclaration(MetaData.Empty, "+",
+				type: new LambdaType(MetaData.Empty,
+					new List<Type>
+					{
+						new PrimaryType(MetaData.Empty, "i8"),
+						new PrimaryType(MetaData.Empty, "i8")
+					},
+					new PrimaryType(MetaData.Empty, "i8"))));
+			// TODO
+			return planet;
+		}
+	}
+}
